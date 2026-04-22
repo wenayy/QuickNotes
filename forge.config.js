3c41b7199dcc35fc912a1ecd89c53d40e369aa5e
vinay joshi <vinay@vinay-2.local>
2026-04-22 09:41:29 +0530

    fix: explicitly set win32 platform for windows makers

--- forge.config.js
+++ forge.config.js
@@ -1,21 +1,28 @@
 module.exports = {
   packagerConfig: {
-    name: 'Quick Notes',
-    executableName: 'quick-notes',
-    icon: './assets/icon', // Will look for icon.icns, icon.ico, icon.png
+    name: 'CacheTray',
+    executableName: 'cachetray',
+    icon: './assets/icon', // Electron-forge handles .icns, .ico based on platform
     asar: true,
   },
   rebuildConfig: {},
   makers: [
     {
       name: '@electron-forge/maker-zip',
-      platforms: ['darwin'],
+      platforms: ['darwin', 'win32'],
     },
     {
       name: '@electron-forge/maker-dmg',
       config: {
-        name: 'Quick Notes',
+        name: 'CacheTray',
         icon: './assets/icon.png',
+      },
+    },
+    {
+      name: '@electron-forge/maker-squirrel',
+      platforms: ['win32'],
+      config: {
+        name: 'cachetray',
       },
     },
   ],
